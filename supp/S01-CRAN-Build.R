ca6d7dea81125a6d7891f209150fb9d89ec12166
Patrice Godard <patrice.godard@ucb.com>
2020-03-30 07:16:25 +0200

    Additional checks prior CRAN submission.

--- supp/S01-CRAN-Build.R
+++ supp/S01-CRAN-Build.R
@@ -4,10 +4,19 @@ library(here)
 devtools::build_vignettes()
 dir.create(here("inst/doc"), showWarnings=FALSE)
 for(f in list.files(here("doc"))){
-   file.copy(file.path(here("doc"), f), file.path(here("vignettes"), f))
-   file.copy(file.path(here("doc"), f), file.path(here("inst/doc"), f))
+   file.copy(
+      file.path(here("doc"), f), file.path(here("vignettes"), f),
+      overwrite=TRUE
+   )
+   file.copy(
+      file.path(here("doc"), f), file.path(here("inst/doc"), f),
+      overwrite=TRUE
+   )
    if(sub("^.*[.]", "", f)=="html"){
-      file.copy(file.path(here("doc"), f), file.path(here("docs"), f))
+      file.copy(
+         file.path(here("doc"), f), file.path(here("docs"), f),
+         overwrite=TRUE
+      )
    }
    file.remove(file.path(here("doc"), f))
 }
